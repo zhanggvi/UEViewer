866058489ee35b3306fa5abad8a27235e513792f
zhanggvi <zhanggvi@gmail.com>
2020-01-09 18:55:29 +0800

    .

--- build.bat
+++ build.bat
@@ -1,5 +1,23 @@
 @echo off
 
-set PATH=%PATH%;D:\git\gildor2\BuildTools\bin
+set CurrDir=%~dp0
+set CopyDir=%1
 
+set PATH=%PATH%;D:\git\BuildTools\bin
+
+
+rem build.
 bash build.sh --64
+
+
+rem copy and auto run.
+if defined CopyDir (
+	echo Input CopyDir: %CopyDir%
+
+	copy /y %CurrDir%\umodel.exe %CopyDir%\umodel.exe
+	copy /y %CurrDir%\SDL2.dll %CopyDir%\SDL2.dll
+
+	cd %CopyDir%
+	start umodel.exe
+	cd %CurrDir%
+)
